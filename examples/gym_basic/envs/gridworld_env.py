ef5a9f7c923bb576158b18650b329417ba664ba7
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-01-21 17:14:10 +0100

    Add policy evaluation code

--- examples/gym_basic/envs/gridworld_env.py
+++ examples/gym_basic/envs/gridworld_env.py
@@ -1,19 +1,27 @@
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+from gym import spaces
+import numpy
 
 
 class GridWorldEnv(gym.Env):
-    metadata = {'render.modes': ['human']}
+    reward_range = (-1, 0)
+    action_space = spaces.Discrete(4)
+    observation_space = spaces.Discrete(15)
 
     def __init__(self):
-        ...
+        gridworld = numpy.arange(16).reshape((4, 4))
+        gridworld[-1, -1] = 0
 
-    def _step(self, action):
-        ...
+        self.P = {0: {}}
+        for a in range(4):
+            self.P[0][a] = [(1.0, 0, 0, True)]
 
-    def _reset(self):
-        ...
-
-    def _render(self, mode='human', close=False):
-        ...
+        for s in gridworld.flat[1:-1]:
+            self.P[s] = {}
+            row, col = numpy.argwhere(gridworld == s)[0]
+            for a, d in zip(range(4), [(-1, 0), (0, 1), (1, 0), (0, -1)]):
+                next_row = max(0, min(row + d[0], 3))
+                next_col = max(0, min(col + d[1], 3))
+                next_state = gridworld[next_row, next_col]
+                done = True if s == 0 else False
+                self.P[s][a] = [(1.0, next_state, -1, done)]
