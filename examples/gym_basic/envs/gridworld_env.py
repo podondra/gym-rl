799631e7f9a0f1a7ad12263f33397ead94bb0bdf
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-01-21 17:53:20 +0100

    Use matrices in policy evaluation code

--- examples/gym_basic/envs/gridworld_env.py
+++ examples/gym_basic/envs/gridworld_env.py
@@ -11,17 +11,17 @@ class GridWorldEnv(gym.Env):
     def __init__(self):
         gridworld = numpy.arange(16).reshape((4, 4))
         gridworld[-1, -1] = 0
-
-        self.P = {0: {}}
-        for a in range(4):
-            self.P[0][a] = [(1.0, 0, 0, True)]
+        # state transition matrix
+        self.P = numpy.zeros((self.action_space.n,
+                              self.observation_space.n,
+                              self.observation_space.n))
 
         for s in gridworld.flat[1:-1]:
-            self.P[s] = {}
             row, col = numpy.argwhere(gridworld == s)[0]
             for a, d in zip(range(4), [(-1, 0), (0, 1), (1, 0), (0, -1)]):
                 next_row = max(0, min(row + d[0], 3))
                 next_col = max(0, min(col + d[1], 3))
-                next_state = gridworld[next_row, next_col]
-                done = True if s == 0 else False
-                self.P[s][a] = [(1.0, next_state, -1, done)]
+                s_prime = gridworld[next_row, next_col]
+                self.P[a, s, s_prime] = 1.0
+
+        self.R = numpy.full((self.action_space.n, self.observation_space.n), -1)
