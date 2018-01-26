f34ee5ec11d7bc0e679ad13acb5c2937493ce1b0
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-01-26 21:28:01 +0100

    Remove QA comment

--- examples/gym_basic/envs/__init__.py
+++ examples/gym_basic/envs/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from gym_basic.envs.gridworld_env import GridWorldEnv
 from gym_basic.envs.windy_gridworld_env import WindyGridWorldEnv
 from gym_basic.envs.cliff_env import CliffEnv
