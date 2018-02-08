9aa0e6580c3397e5af238ef2cea4a8814d95b21f
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-02-08 20:40:15 +0100

    Move policy improvement to script

--- examples/dynamic_programming.py
+++ examples/dynamic_programming.py
@@ -27,3 +27,26 @@ def policy_evaluation(policy, env, gamma=1.0, epsilon=1e-5):
             delta = max(delta, numpy.abs(v - V[s]))
         if delta < epsilon:
             return V
+
+
+def policy_improvement(env, V, gamma=1.0):
+    """Policy improvement algorithm.
+
+    Return transition matrix of policy given by state-value function V.
+
+    env is a OpenAI gym environment transition dynamics as attribute P.
+    V is numpy array of state-values.
+    gamma is a discount rate.
+    """
+    # construct empty transition matrix
+    policy = numpy.zeros((env.observation_space.n, env.action_space.n))
+    for s in range(env.observation_space.n):
+        # for each state construct state-action value
+        action_values = numpy.zeros(env.action_space.n)
+        for a in range(env.action_space.n):
+            action_values[a] = env.P[a, s] @ (env.R[a, s] + gamma * V)
+        # choose the best action
+        a = numpy.argmax(action_values)
+        # set probability 1 for that action
+        policy[s, a] = 1.0
+    return policy
--- tests/test_dynamic_programming.py
+++ tests/test_dynamic_programming.py
@@ -1,16 +1,37 @@
-import gym
-import gym_gridworlds   # noqa
+import pytest
 import numpy
 from examples import dynamic_programming
 
 
-def test_policy_evaluation():
-    env = gym.make('Gridworld-v0')
+@pytest.fixture
+def gridworld():
+    import gym
+    import gym_gridworlds   # noqa
+    return gym.make('Gridworld-v0')
+
+
+@pytest.fixture
+def true_state_values():
+    return numpy.array(
+            [0, -14, -20, -22, -14, -18, -20, -20, -20, -20, -18, -14, -22,
+                -20, -14]
+            )
+
+
+def test_policy_evaluation(gridworld, true_state_values):
     # define transition for stochastic policy
-    stochastic = numpy.ones((env.observation_space.n, env.action_space.n))
-    stochastic /= env.action_space.n
+    stochastic = numpy.ones(
+            (gridworld.observation_space.n, gridworld.action_space.n)
+            )
+    stochastic /= gridworld.action_space.n
     # evaluate the stochastic policy
-    V = dynamic_programming.policy_evaluation(stochastic, env)
-    optimal_V = [0, -14, -20, -22, -14, -18, -20, -20, -20, -20, -18, -14, -22,
-                 -20, -14]
-    assert numpy.allclose(V, optimal_V)
+    V = dynamic_programming.policy_evaluation(stochastic, gridworld)
+    assert numpy.allclose(V, true_state_values)
+
+
+def test_policy_improvement(gridworld, true_state_values):
+    policy = dynamic_programming.policy_improvement(
+            gridworld, true_state_values
+            )
+    # make sure that probabilities sum up to 1
+    assert numpy.all(policy.sum(axis=1) == 1)
